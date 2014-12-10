04421bb17d00d1b703d3c466af4068d6da570eb3
Christoffer Artmann <christoffer@artmann.co>
2014-12-11 00:04:23 +0100

    Article Admin WIP

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ArticlesController < ApplicationController
+	before_action :set_article, :only => [ :show, :edit, :update, :destroy ]
+
 	def index
 		@articles = Article.all.order(:created_at)
 	end
@@ -16,9 +18,12 @@ class Admin::ArticlesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
 	private
 	def article_params
-		params.require(:article).permit( :title, :excerpt, :content, :featured_image )
+		params.require(:article).permit( :title, :excerpt, :content, :featured_image, :published, :published_at )
 	end
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, :only => [ :show, :edit, :update, :destroy ]
 	def index
 		@articles = Article.published.order("published_at DESC")
 	end
+
 	def show
+#		@article = Article.friendly.find(params[:id])
+	end
+
+	def edit
+	end
+	
+	
+
+	private
+	def set_article
 		@article = Article.friendly.find(params[:id])
 	end
+
+
 end
